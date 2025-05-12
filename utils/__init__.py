1fbdd20f0b0592ce381c80ac24c2b7fa67f54c5d
FadyNF <fadynf05@gmail.com>
2025-05-12 15:52:37 +0300

    Add scaling and skewness treatment utilities
    - Introduced `scale_features` function in `scaling.py` to standardize or normalize numerical features in a DataFrame, excluding specified columns.
    - Added `treat_skewness` function in `skewness.py` to address skewness in continuous features using various transformation techniques based on skewness severity.
    - Updated `__init__.py` to include new utilities for scaling and skewness treatment.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,4 @@
 from .preprocessing import clean_missing_values, look_for_outliers, handle_outliers_iqr
 from .encoding import encode_features
+from .skewness import treat_skewness
+from .scaling import scale_features
